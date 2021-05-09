2567967a9de84335709e73ca3937449a89a1e09f
GGiulianni <nicoligiu2@gmai.com>
2021-05-09 18:11:11 -0300

    update projects

---
+++ FrontEnd/cardsHeroes/html/js/script.js
@@ -0,0 +1,11 @@
+function setVisible(id) {
+    document.getElementById(id).style.visibility = "visible";
+}
+
+function setHidden(id) {
+    document.getElementById(id).style.visibility = "hidden";
+}
+
+
+/*Este script tiene derechos autorales de Valentina Varela,
+me lo copié para solventar un problema de visibilidad del botón.*/ 
